dcbc3a92ae58b264f8aeecb07d491fb630aa6896
Rei Kato <reireinio@gmail.com>
2021-11-28 11:37:15 +0900

    Initial Commit

---
+++ o/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  o
+//
+//  Created by 加藤　怜 on 2021/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
